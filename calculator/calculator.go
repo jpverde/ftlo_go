7938217e337a2cbb788e96f5e5621487938b6da5
John Perez <mrjohnjairo10@gmail.com>
2024-08-30 12:06:58 -0500

    Add calculator package and tests

---
+++ calculator/calculator.go
@@ -0,0 +1,8 @@
+// Package calculator does simple calculations
+package calculator
+
+// Add takes two numbers and return the result of the adding
+// them together
+func Add(a, b float64) float64 {
+	return a + b
+}
