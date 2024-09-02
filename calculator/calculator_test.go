d5bcee3183e45e5b140db78613771c181bd22b10
John Perez <mrjohnjairo10@gmail.com>
2024-09-02 10:57:39 -0500

    Add multiply function and test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,10 +5,28 @@ import (
 	"testing"
 )
 
-func AddTest(t *testing.T) {
+func TestAdd(t *testing.T) {
 	t.Parallel()
 	var want float64 = 4
 	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestSubstract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 6
+	got := calculator.Multiply(2, 3)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
